25425ea0b33fec7b58e8fbb57b8242cf330c798a
rleee <danlee0150@gmail.com>
2020-01-30 22:35:19 +0800

    add root route

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+require("./routes/testRoutes")(app);
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
