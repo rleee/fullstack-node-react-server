7458e87947a1f3eeac706576a5717ee1466fc5aa
rleee <danlee0150@gmail.com>
2020-02-01 13:41:26 +0800

    make use of materialize-css

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="brand-logo">emaily</a>
+          <ul className="right">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
