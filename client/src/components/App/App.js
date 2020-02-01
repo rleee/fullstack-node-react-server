49f9f0a84268b435fdcbec633604144eea581f72
rleee <danlee0150@gmail.com>
2020-02-01 13:22:39 +0800

    make use of redux, react-router-dom

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "../Header/Header";
+const Dashboard = () => <h2>Dashboard</h2>;
+const Survey = () => <h2>Survey</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi this is App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={Survey} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
