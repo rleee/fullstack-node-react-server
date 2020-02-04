34e084d1cea093bff9728a958abbe85df1dd3b7d
rleee <danlee0150@gmail.com>
2020-02-04 22:20:02 +0800

    make use of send grid and test it from chrome console

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from "redux-thunk";
 import reducers from "./reducers";
 import App from "./components/App/App";
 
+// only for testing on client google chrome console
+import axios from "axios";
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
